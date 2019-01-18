31249bd238abc75a022a05652ee3126fa9136575
FransuaSzatowalo <machpawel95@gmail.com>
2019-01-18 21:38:20 +0100

    Added autocomplete for services in invoice create

--- src/app/invoice/create/create.component.ts
+++ src/app/invoice/create/create.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from '../invoice.service';
 import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
-import { debounceTime, switchMap, tap, finalize } from 'rxjs/operators';
+import { startWith, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 export interface Service {
+  _id: String,
   name: String,
   description: String,
   price: Number
@@ -15,12 +17,20 @@ export interface Service {
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
-  filteredServices: Service[] = [];
+  filteredServices: Observable<Service[]>;
+  services: Service[] = [];
+  selectedServices: Service[];
   invoiceForm: FormGroup;
-  servicesForm: FormGroup;
-  isLoading = false;
+  serviceInput = new FormControl('');
 
-  constructor(private invoiceService: InvoiceService, private fb: FormBuilder) { }
+  constructor(private invoiceService: InvoiceService, private fb: FormBuilder) { 
+    this.getServices();
+    this.filteredServices = this.serviceInput.valueChanges
+      .pipe(
+        startWith(''),
+        map(service => service ? this._filteredServices(service) : this.services.slice())
+      );
+  }
   
   ngOnInit() { 
     this.invoiceForm = this.fb.group({
@@ -29,22 +39,19 @@ export class CreateComponent implements OnInit {
       dueDate: null,
       tax: null,
     });
-
-    this.servicesForm = this.fb.group({
-      serviceInput: null
-    })
-
-    this.servicesForm
-      .get('serviceInput')
-      .valueChanges
-      .pipe(
-        debounceTime(300),
-        tap(() => this.isLoading = true),
-        switchMap(value => this.invoiceService.searchServices(value)
-          .pipe(finalize(() => this.isLoading = false))
-        )
-      )
-      .subscribe(services => this.filteredServices = services);
   }
 
+  private _filteredServices(value: string): Service[] {
+    const filterValue = value.toLowerCase();
+    return this.services.filter(service => service.name.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+  getServices() {
+    this.invoiceService.getServices()
+      .subscribe(services => this.services = services)
+  }
+
+  Add() {
+    this.selectedServices.push(this.serviceInput.value);
+  }
 }
--- src/app/invoice/invoice.service.ts
+++ src/app/invoice/invoice.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators'
+import { tap } from 'rxjs/operators';
+
+export interface IServiceResponse {
+  total: number,
+  results: Service[]
+}
+
+export class Service {
+  constructor(
+    public _id: String,
+    public name: String,
+    public description: String,
+    public price: Number
+  ) {}
+}
 
 @Injectable({
   providedIn: 'root'
@@ -42,11 +56,15 @@ export class InvoiceService {
     return this.http.get<any[]>(this.serviceUrl);
   }
 
-  searchServices(name: string): Observable<any[]> {
-    if (!name.trim()){
-      return of([]);
-    }
-    return this.http.get<any[]>(`${this.serviceUrl}/name?${name}`);
+  searchServices(name: string): Observable<IServiceResponse> {
+    return this.http.get<IServiceResponse>(`${this.serviceUrl}/name/${name}`)
+      .pipe(
+        tap((response: IServiceResponse) => {
+          response.results = response.results
+            .map(service => new Service(service._id, service.name, service.description, service.price))
+            return response
+        })
+      );
   }
   
 }
